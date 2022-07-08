e2416a7eb16863c671dd5884670e61dfab1112c6
Jeffrey <jeffreyally@gmail.com>
2022-07-08 13:26:32 -0400

    update

--- src/Blah.js
+++ src/Blah.js
@@ -1,21 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 export function Blah(props) {
 
-  const [characterInfo, setCharacterInfo] = useState(
-    {
-      name: '',
-      height: '',
-      homeWorld: '',
-      films: [],
-      loadedCharacter: false
-    })
+  const [data, setData] = useState(null)
+  
 
-  let getCharacter = () => {
-    setCharacterInfo({ ...characterInfo, loadedCharacter:true, name: 'Luke', height: 172, homeWorld: 'Tatooine', films: [1, 2, 'Last Jedi'] })
-  }
+  let URL = `https://swapi.dev/api/people/${Math.ceil(Math.random() * 82)}`;
+ 
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
+  useEffect(
+    ()=>{
+    console.log(data,'data variable changed line 18')
+    
+    
+  
+  },
+    [data]
+  )
+
+
+
+
+
+
+  let getCharacter = async () => {
+    fetch(URL)
+      .then((response) => {
+        console.log("using this URL here:", URL);
+        
+        return response.json();
+      })
+      .then((responseAsJson) => {
+        // Do stuff with the JSONified response
+        console.log("The response", responseAsJson);
+        
+        setData(responseAsJson);
+        
+        return responseAsJson;
+      })
+      .catch((error) => {
+        console.log("Looks like there was a problem: \n", error);
+      });
+  };
+
 
 
 
@@ -25,31 +58,33 @@ export function Blah(props) {
       <div className='StarWars d-flex flex-column align-items-center justify-content-center' style={{ width: '100vw', height: '100vh' }}>
 
         {
-          characterInfo.loadedCharacter &&
+          data &&
           <span>
-            <h1>{characterInfo.name}</h1>
+            <h1>{data.name}</h1>
             <br></br>
-            <h5>{characterInfo.height} cm</h5>
-            <h5>{characterInfo.homeWorld}</h5>
-            <ul>
-              {characterInfo.films.map((film) => {
-                console.log(film)
-                return <li>{film}</li>
+            <h5>Height: {data.height} cm</h5>
+            <h5>Home World: {data.homeworld}</h5>
+            <h5>Film Appearances</h5>
+            <ol type="1">
+              {data.films.map((film,index) => {
+                
+                return <li key={index}>{film}</li>
               })}
 
 
-            </ul>
-          </span>
+            </ol>
+          </span> 
+            
         }
-< button type="button" className="btn btn-warning btn-lg mt-5" style={{ backgroundColor: 'black', color: 'yellow' }
-      }
-        onClick={() => {
-          getCharacter();
-        }}> Randomize Character</button >
+        < button type="button" className="btn btn-warning btn-lg mt-5" style={{ backgroundColor: 'black', color: 'yellow' }
+        }
+          onClick={() => {
+            getCharacter();
+          }}> Randomize Character</button >
       </div>
 
 
-      
+
 
 
 
